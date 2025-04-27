4c3ecde08fd3853ee48878244b562006a78501e5
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:21:00 +0300

    Receive <> user input (question title, score, and associated quiz ID) from an HTTP POST request

--- createQuestion.php
+++ createQuestion.php
@@ -4,7 +4,11 @@ try {
     // Include the database connection file
     include("./connection.php");
 
-    
+    // Receive user input (question title, score, and associated quiz ID) from an HTTP POST request
+    $quTitle = $_POST["quTitle"];
+    $quScore = $_POST["quScore"];
+    $quizID = $_POST["quizID"];
+
     
     
 } catch (\Throwable $th) {
