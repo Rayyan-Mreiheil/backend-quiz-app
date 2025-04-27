a7bcc03a7bbfe15cfa730552abc358ecf41c0929
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:21:09 +0300

    Create <> register.php file

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ try {
     // Database connection parameters
     $host = "localhost";       // Host where the database server is running
     $port = 3306;              // Default MySQL port
-    $dbname = "quizappdb";     // Name of the database you want to connect to
+    $dbname = "quizappdb";     // Name of the database I want to connect to
     $user = "rayyan";          // Username to access the database
     $pass = "thanksgod@rayyan11"; // Password for the database user
 
