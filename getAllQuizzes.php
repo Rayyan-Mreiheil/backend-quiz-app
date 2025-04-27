26d2e779c143402b7f357906916e307d78dfd8d8
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:57:50 +0300

    Prepare <> an SQL statement to select all quizzes from the 'quizzes' table

--- getAllQuizzes.php
+++ getAllQuizzes.php
@@ -4,6 +4,9 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Prepare an SQL statement to select all quizzes from the 'quizzes' table
+    $query = $connection->prepare("SELECT * FROM quizzes");
+    
     
 } catch (\Throwable $th) {
     
