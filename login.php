1d1dc393c3e8aaf033361cddee41179c716ef6db
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:38:46 +0300

    Prepare <> an SQL statement to select a user matching the provided email and password

--- login.php
+++ login.php
@@ -9,6 +9,10 @@ try {
     $userEmail = $_POST["userEmail"];
     $userPassword = $_POST["userPassword"];
 
+    // Prepare an SQL statement to select a user matching the provided email and password
+    $query = $connection->prepare("SELECT * FROM users WHERE userEmail = :userEmail AND userPassword = :userPassword");
+
+    
     
 
 } catch (\Throwable $th) {
