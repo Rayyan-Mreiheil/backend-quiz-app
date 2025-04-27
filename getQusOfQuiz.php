396ecc4f7deb26c0e5f9b525a5bd45e78227b546
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:29:29 +0300

    Bind <> the quiz ID to the SQL statement to prevent SQL injection

--- getQusOfQuiz.php
+++ getQusOfQuiz.php
@@ -10,6 +10,10 @@ try {
     // Prepare an SQL statement to select all questions that belong to a specific quiz ID
     $query = $connection->prepare("SELECT * FROM questions WHERE quizID = :quizID");
 
+    // Bind the quiz ID to the SQL statement to prevent SQL injection
+    $query->bindParam(":quizID", $quizID, PDO::PARAM_INT);
+
+    
     
 } catch (\Throwable $th) {
    
