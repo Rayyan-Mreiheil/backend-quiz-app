0f106d59da6c35923dcedc84d5f6aa08a3ecc2ca
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:38:12 +0300

    Prepare <> an SQL statement to update the question title for a specific question ID

--- editQuestion.php
+++ editQuestion.php
@@ -8,7 +8,9 @@ try {
     $quTitle = $_POST["quTitle"];
     $quID = $_POST["quID"];
 
-    
+    // Prepare an SQL statement to update the question title for a specific question ID
+    $query = $connection->prepare("UPDATE questions SET quTitle = :quTitle WHERE quID = :quID");
+
     
     
 } catch (\Throwable $th) {
