86c76ea4da9c4c84c43a68ffde34bcce6973d172
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:46:03 +0300

    Receive <> user input (quiz title and description) from an HTTP POST request

--- createQuiz.php
+++ createQuiz.php
@@ -4,6 +4,10 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive user input (quiz title and description) from an HTTP POST request
+    $quizTitle = $_POST["quizTitle"];
+    $quizDescription = $_POST["quizDescription"];
+
    
 } catch (\Throwable $th) {
     
