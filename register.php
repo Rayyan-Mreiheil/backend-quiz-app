15362301b8d502fdff5e6819961881a22f660f67
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:23:03 +0300

    Receive <> user input from an HTTP POST request

--- register.php
+++ register.php
@@ -4,6 +4,12 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive user input from an HTTP POST request
+    $userName = $_POST["userName"];
+    $userEmail = $_POST["userEmail"];
+    $userPassword = $_POST["userPassword"];
+    $userRole = $_POST["userRole"];
+
     
 } catch (\Throwable $th) {
     
