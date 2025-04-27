2c5198e7f9167bce2e3dcb094d39f996e0fd3cc2
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:28:46 +0300

    Prepare <> an SQL statement to select all questions that belong to a specific quiz ID

--- getQusOfQuiz.php
+++ getQusOfQuiz.php
@@ -7,7 +7,9 @@ try {
     // Receive the quiz ID from an HTTP POST request to fetch its questions
     $quizID = $_POST["quizID"];
 
-    
+    // Prepare an SQL statement to select all questions that belong to a specific quiz ID
+    $query = $connection->prepare("SELECT * FROM questions WHERE quizID = :quizID");
+
     
 } catch (\Throwable $th) {
    
