0aba56fc792da6bb40d16621a3b823fe640d6b6c
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:24:42 +0300

    Prepare <> an SQL statement to insert a new user into the 'users' table

--- register.php
+++ register.php
@@ -10,6 +10,9 @@ try {
     $userPassword = $_POST["userPassword"];
     $userRole = $_POST["userRole"];
 
+    // Prepare an SQL statement to insert a new user into the 'users' table
+    $query = $connection->prepare("INSERT INTO users (userName, userEmail, userPassword, userRole) VALUES (:userName, :userEmail, :userPassword, :userRole)");
+
     
 } catch (\Throwable $th) {
     
