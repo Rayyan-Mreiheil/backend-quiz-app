daeefff0beffad53d04f9b8e57ecab4c06983049
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-27 23:38:05 +0300

    Receive <> user input (email and password) from an HTTP POST request

--- login.php
+++ login.php
@@ -5,6 +5,10 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive user input (email and password) from an HTTP POST request
+    $userEmail = $_POST["userEmail"];
+    $userPassword = $_POST["userPassword"];
+
     
 
 } catch (\Throwable $th) {
