940710237fdfe6022b05404603c822bea2fbccb7
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:45:51 +0300

    Receive <> the question ID from an HTTP POST request to delete a specific question

--- deleteQuestion.php
+++ deleteQuestion.php
@@ -4,6 +4,9 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive the question ID from an HTTP POST request to delete a specific question
+    $quID = $_POST["quID"];
+
     
 } catch (\Throwable $th) {
     
