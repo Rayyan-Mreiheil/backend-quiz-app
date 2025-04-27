0b88572b512decc6d2f77df8773df1bd3b971a3c
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:06:24 +0300

    Receive <> user input (quiz ID and new quiz description) from an HTTP POST request

--- editQuiz.php
+++ editQuiz.php
@@ -4,6 +4,10 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive user input (quiz ID and new quiz description) from an HTTP POST request
+    $quizeID = $_POST["quizeID"];
+    $quizDescription = $_POST["quizDescription"];
+
     
 } catch (\Throwable $th) {
     
