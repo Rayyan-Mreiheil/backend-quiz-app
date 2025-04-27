b6d98c391f77df43125655b15e0d39aa29d224bd
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:37:35 +0300

    Receive <> user input (question title and question ID) from an HTTP POST request

--- editQuestion.php
+++ editQuestion.php
@@ -4,6 +4,10 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive user input (question title and question ID) from an HTTP POST request
+    $quTitle = $_POST["quTitle"];
+    $quID = $_POST["quID"];
+
     
     
     
