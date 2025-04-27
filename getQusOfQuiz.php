8667fc190b6c0663fe63dad4ef298dbce36171f9
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:28:11 +0300

    Receive <> the quiz ID from an HTTP POST request to fetch its questions

--- getQusOfQuiz.php
+++ getQusOfQuiz.php
@@ -4,6 +4,9 @@ try {
     // Include the database connection file
     include("./connection.php");
 
+    // Receive the quiz ID from an HTTP POST request to fetch its questions
+    $quizID = $_POST["quizID"];
+
     
     
 } catch (\Throwable $th) {
