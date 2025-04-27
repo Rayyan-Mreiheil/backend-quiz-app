b031088ad496fc7ac2dd9095d4f2a53c5efc004e
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-28 00:14:42 +0300

    Receive <> the quiz ID to delete from an HTTP POST request

--- deleteQuiz.php
+++ deleteQuiz.php
@@ -4,7 +4,9 @@ try {
     // Include the database connection file
     include("./connection.php");
 
-    
+    // Receive the quiz ID to delete from an HTTP POST request
+    $quizeID = $_POST["quizeID"];
+
     
 } catch (\Throwable $th) {
     
